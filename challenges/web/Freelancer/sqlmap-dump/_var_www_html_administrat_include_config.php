43dc4aace6e784fe1bcb0e981245f0b67297ceba
Fahmi <65661058+fahmifj@users.noreply.github.com>
2021-06-12 08:21:37 +0700

    Clean up challenges for new format

--- challenges/web/Freelancer/sqlmap-dump/_var_www_html_administrat_include_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = new mysqli("localhost", "db_user", "Str0ngP4ss", "freelancer");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
